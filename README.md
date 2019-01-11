aa48ec293e7e3f0f6ab3cda59e26785fcd27ba5e
runzexia <runzexia@yunify.com>
2019-01-11 13:54:57 +0800

    add step create api

--- README.md
+++ README.md
@@ -8,4 +8,67 @@ Install `dep`, `kustomize` and `kubebuilder`. [Link](https://book.kubebuilder.io
 
 Run `kubebuilder init --domain kubesphere.io --owner "The KubeSphere Authors"` to init project
 
+## Step 3
+
+Run `kubebuilder create api --group iam --version v1alpha2 --kind Workspace --namespaced=false` to create api.
+
+```bash
+$ kubebuilder create api --group iam --version v1alpha2 --kind Workspace --namespaced=false
+Create Resource under pkg/apis [y/n]?
+y
+Create Controller under pkg/controller [y/n]?
+y
+Writing scaffold for you to edit...
+pkg/apis/iam/v1alpha2/workspace_types.go
+pkg/apis/iam/v1alpha2/workspace_types_test.go
+pkg/controller/workspace/workspace_controller.go
+pkg/controller/workspace/workspace_controller_test.go
+Running make...
+go generate ./pkg/... ./cmd/...
+go fmt ./pkg/... ./cmd/...
+go vet ./pkg/... ./cmd/...
+go run vendor/sigs.k8s.io/controller-tools/cmd/controller-gen/main.go all
+CRD manifests generated under '/Users/runzexia/go/src/github.com/runzexia/kubesphere-crd-sample/config/crds'
+RBAC manifests generated under '/Users/runzexia/go/src/github.com/runzexia/kubesphere-crd-sample/config/rbac'
+go test ./pkg/... ./cmd/... -coverprofile cover.out
+?   	github.com/runzexia/kubesphere-crd-sample/pkg/apis	[no test files]
+?   	github.com/runzexia/kubesphere-crd-sample/pkg/apis/iam	[no test files]
+ok  	github.com/runzexia/kubesphere-crd-sample/pkg/apis/iam/v1alpha2	9.310s	coverage: 23.4% of statements
+?   	github.com/runzexia/kubesphere-crd-sample/pkg/controller	[no test files]
+ok  	github.com/runzexia/kubesphere-crd-sample/pkg/controller/workspace	10.674s	coverage: 67.6% of statements
+?   	github.com/runzexia/kubesphere-crd-sample/pkg/webhook	[no test files]
+?   	github.com/runzexia/kubesphere-crd-sample/cmd/manager	[no test files]
+go build -o bin/manager github.com/runzexia/kubesphere-crd-sample/cmd/manager
+```
+
+Run `kubebuilder create api --group devops --version v1alpha2 --kind DevOpsProject --namespaced=false` to create api.
+
+
+```bash
+$ kubebuilder create api --group devops --version v1alpha2 --kind DevOpsProject --namespaced=false
+Create Resource under pkg/apis [y/n]?
+y
+Create Controller under pkg/controller [y/n]?
+y
+Writing scaffold for you to edit...
+pkg/apis/devops/v1alpha2/devopsproject_types.go
+pkg/apis/devops/v1alpha2/devopsproject_types_test.go
+pkg/controller/devopsproject/devopsproject_controller.go
+pkg/controller/devopsproject/devopsproject_controller_test.go
+Running make...
+go generate ./pkg/... ./cmd/...
+go fmt ./pkg/... ./cmd/...
+go vet ./pkg/... ./cmd/...
+go run vendor/sigs.k8s.io/controller-tools/cmd/controller-gen/main.go all
+CRD manifests generated under '/Users/runzexia/go/src/github.com/runzexia/kubesphere-crd-sample/config/crds'
+RBAC manifests generated under '/Users/runzexia/go/src/github.com/runzexia/kubesphere-crd-sample/config/rbac'
+go test ./pkg/... ./cmd/... -coverprofile cover.out
+?   	github.com/runzexia/kubesphere-crd-sample/pkg/apis	[no test files]
+?   	github.com/runzexia/kubesphere-crd-sample/pkg/apis/devops	[no test files]
+ok  	github.com/runzexia/kubesphere-crd-sample/pkg/apis/devops/v1alpha2	7.816s	coverage: 23.4% of statements
+?   	github.com/runzexia/kubesphere-crd-sample/pkg/controller	[no test files]
+ok  	github.com/runzexia/kubesphere-crd-sample/pkg/controller/devopsproject	9.255s	coverage: 67.6% of statements
+?   	github.com/runzexia/kubesphere-crd-sample/pkg/webhook	[no test files]
+?   	github.com/runzexia/kubesphere-crd-sample/cmd/manager	[no test files]
+```
 
---
+++ pkg/controller/workspace/workspace_controller.go
@@ -0,0 +1,169 @@
+/*
+Copyright 2019 The KubeSphere Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package workspace
+
+import (
+	"context"
+	"reflect"
+
+	iamv1alpha2 "github.com/runzexia/kubesphere-crd-sample/pkg/apis/iam/v1alpha2"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+var log = logf.Log.WithName("controller")
+
+/**
+* USER ACTION REQUIRED: This is a scaffold file intended for the user to modify with their own Controller
+* business logic.  Delete these comments after modifying this file.*
+ */
+
+// Add creates a new Workspace Controller and adds it to the Manager with default RBAC. The Manager will set fields on the Controller
+// and Start it when the Manager is Started.
+func Add(mgr manager.Manager) error {
+	return add(mgr, newReconciler(mgr))
+}
+
+// newReconciler returns a new reconcile.Reconciler
+func newReconciler(mgr manager.Manager) reconcile.Reconciler {
+	return &ReconcileWorkspace{Client: mgr.GetClient(), scheme: mgr.GetScheme()}
+}
+
+// add adds a new Controller to mgr with r as the reconcile.Reconciler
+func add(mgr manager.Manager, r reconcile.Reconciler) error {
+	// Create a new controller
+	c, err := controller.New("workspace-controller", mgr, controller.Options{Reconciler: r})
+	if err != nil {
+		return err
+	}
+
+	// Watch for changes to Workspace
+	err = c.Watch(&source.Kind{Type: &iamv1alpha2.Workspace{}}, &handler.EnqueueRequestForObject{})
+	if err != nil {
+		return err
+	}
+
+	// TODO(user): Modify this to be the types you create
+	// Uncomment watch a Deployment created by Workspace - change this for objects you create
+	err = c.Watch(&source.Kind{Type: &appsv1.Deployment{}}, &handler.EnqueueRequestForOwner{
+		IsController: true,
+		OwnerType:    &iamv1alpha2.Workspace{},
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+var _ reconcile.Reconciler = &ReconcileWorkspace{}
+
+// ReconcileWorkspace reconciles a Workspace object
+type ReconcileWorkspace struct {
+	client.Client
+	scheme *runtime.Scheme
+}
+
+// Reconcile reads that state of the cluster for a Workspace object and makes changes based on the state read
+// and what is in the Workspace.Spec
+// TODO(user): Modify this Reconcile function to implement your Controller logic.  The scaffolding writes
+// a Deployment as an example
+// Automatically generate RBAC rules to allow the Controller to read and write Deployments
+// +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=apps,resources=deployments/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=iam.kubesphere.io,resources=workspaces,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=iam.kubesphere.io,resources=workspaces/status,verbs=get;update;patch
+func (r *ReconcileWorkspace) Reconcile(request reconcile.Request) (reconcile.Result, error) {
+	// Fetch the Workspace instance
+	instance := &iamv1alpha2.Workspace{}
+	err := r.Get(context.TODO(), request.NamespacedName, instance)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Object not found, return.  Created objects are automatically garbage collected.
+			// For additional cleanup logic use finalizers.
+			return reconcile.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		return reconcile.Result{}, err
+	}
+
+	// TODO(user): Change this to be the object type created by your controller
+	// Define the desired Deployment object
+	deploy := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      instance.Name + "-deployment",
+			Namespace: "default",
+		},
+		Spec: appsv1.DeploymentSpec{
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{"deployment": instance.Name + "-deployment"},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{Labels: map[string]string{"deployment": instance.Name + "-deployment"}},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  "nginx",
+							Image: "nginx",
+						},
+					},
+				},
+			},
+		},
+	}
+	if err := controllerutil.SetControllerReference(instance, deploy, r.scheme); err != nil {
+		return reconcile.Result{}, err
+	}
+
+	// TODO(user): Change this for the object type created by your controller
+	// Check if the Deployment already exists
+	found := &appsv1.Deployment{}
+	err = r.Get(context.TODO(), types.NamespacedName{Name: deploy.Name, Namespace: deploy.Namespace}, found)
+	if err != nil && errors.IsNotFound(err) {
+		log.Info("Creating Deployment", "namespace", deploy.Namespace, "name", deploy.Name)
+		err = r.Create(context.TODO(), deploy)
+		if err != nil {
+			return reconcile.Result{}, err
+		}
+	} else if err != nil {
+		return reconcile.Result{}, err
+	}
+
+	// TODO(user): Change this for the object type created by your controller
+	// Update the found object and write the result back if there are any changes
+	if !reflect.DeepEqual(deploy.Spec, found.Spec) {
+		found.Spec = deploy.Spec
+		log.Info("Updating Deployment", "namespace", deploy.Namespace, "name", deploy.Name)
+		err = r.Update(context.TODO(), found)
+		if err != nil {
+			return reconcile.Result{}, err
+		}
+	}
+	return reconcile.Result{}, nil
+}
