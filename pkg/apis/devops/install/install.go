57d7f2ed0eee6407846fd3d411033fdf146b7ec4
runzexia <runzexia@yunify.com>
2019-01-23 11:06:25 +0800

    add install func

---
+++ pkg/apis/devops/install/install.go
@@ -0,0 +1,13 @@
+package install
+
+
+import (
+	"github.com/runzexia/kubesphere-crd-sample/pkg/apis/devops/v1alpha2"
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+)
+
+func Install(scheme *runtime.Scheme) {
+	utilruntime.Must(v1alpha2.AddToScheme(scheme))
+	utilruntime.Must(scheme.SetVersionPriority(v1alpha2.SchemeGroupVersion))
+}
